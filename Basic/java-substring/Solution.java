974eefe29e643a7036032b773bbda6d88c8e514a
Oseamiya <85782575+oseamiya@users.noreply.github.com>
2022-05-01 12:16:17 +0545

    Create Solution.java

---
+++ Basic/java-substring/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        // Using substring method of string we can get new string from beginning index to end index.
+        System.out.println(S.substring(start, end));
+    }
+}
